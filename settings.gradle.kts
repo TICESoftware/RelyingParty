152e068f57728bed226fc575907769f7515079ec
Tim Schauder <temscha@live.com>
2024-08-14 16:40:27 +0200

    Add signature check for sdjwt

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        google()
         mavenCentral()
         maven {
             url = uri("https://jitpack.io")
